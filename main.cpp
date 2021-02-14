4b2a09452469a08ac5beb8c1dab99a516371f5a7
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-02-14 16:01:54 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string eil1, eil2, eil3;
+    std::string name;
+    std::cout << "Iveskite savo varda: ";
+    std::cin >> name;
+    int a = name.length();
+    // 1 ir 5 eilutes
+    for (int i=0; i<a+14; i++)
+        eil1 += "*";
+    // 2 ir 4 eilutes
+    eil2 = "*";
+    for (int i=0; i<a+12; i++)
+        eil2+=" ";
+    eil2 += "*";
+    // 3 eilute
+    eil3 = "* Sveikas, "+ name + "! *";
+    
+    std::cout << eil1 << std::endl;
+    std::cout << eil2 << std::endl;
+    std::cout << eil3 << std::endl;
+    std::cout << eil2 << std::endl;
+    std::cout << eil1 << std::endl;
+}
